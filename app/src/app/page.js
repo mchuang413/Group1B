f35c4f624a6e7f609e8568750224e77a244a7db9
k-coding-k <k.coding.98@gmail.com>
2024-07-24 14:37:47 -0500

    Landing Page

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <button className="btn btn-active btn-primary">This is my home</button>
+      <div className="center">
+        <Image width={800} height={747} src="/medvice.png" />
+      </div>
     </>
   );
 }
